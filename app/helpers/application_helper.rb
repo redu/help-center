159fcdc979d8644b98e815ea384cf132254e8569
jessicabarbalho <jessica@redu.com.br>
2012-11-01 09:44:34 -0300

    Top_questions partial with path

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def topic_path_html(topic)
+    html = ""
+    ancestors = topic.ancestors
+
+    ancestors.each do |parent|
+      html << parent.title
+      html << (parent == ancestors.last ? "" : " > ")
+    end
+
+    html
+  end
 end

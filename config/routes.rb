70acd17c6d2f1e2f156fbe31d4adcab9b5678a2e
jessicabarbalho <jessica@redu.com.br>
2012-11-19 12:38:52 -0300

    Fix creation route for topics

--- config/routes.rb
+++ config/routes.rb
@@ -7,15 +7,15 @@ HelpCenter::Application.routes.draw do
 
   get "/:id" => "topics#show", as: :show_root_topic
 
-  resources :topics, only: [:index]
-  get "/:root_id/:id" => "topics#show", as: :show_topic
-
   scope "/admin" do
     resources :topics, except: [:index, :show]
     resources :basic_guides, only: [:new, :create, :update]
     resources :faqs, only: [:new, :create, :update]
     resources :guides, only: [:new, :create, :update]
   end
+
+  resources :topics, only: [:index]
+  get "/:root_id/:id" => "topics#show", as: :show_topic
 
   ActionDispatch::Routing::Translator.translate_from_file('lang/i18n-routes.yml', { :no_prefixes => true })
 end
